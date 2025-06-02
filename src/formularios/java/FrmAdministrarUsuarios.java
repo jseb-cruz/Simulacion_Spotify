c85ca23dd2ade2b63057c5c501fd79921f16d566
jseb-cruz <09sebastiancruz06@gmail.com>
2025-06-02 00:54:00 -0500

    creacion de formulario

--- src/formularios/java/FrmAdministrarUsuarios.java
+++ src/formularios/java/FrmAdministrarUsuarios.java
@@ -1,4 +1,3 @@
-package formularios.java;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -8,12 +7,12 @@ package formularios.java;
  *
  * @author User
  */
-public class MenuPrincipal extends javax.swing.JFrame {
+public class FrmAdministrarUsuarios extends javax.swing.JInternalFrame {
 
     /**
-     * Creates new form MenuPrincipal
+     * Creates new form FrmAdministrarUsuarios
      */
-    public MenuPrincipal() {
+    public FrmAdministrarUsuarios() {
         initComponents();
     }
 
@@ -59,20 +58,20 @@ public class MenuPrincipal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmAdministrarUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmAdministrarUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmAdministrarUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmAdministrarUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MenuPrincipal().setVisible(true);
+                new FrmAdministrarUsuarios().setVisible(true);
             }
         });
     }
